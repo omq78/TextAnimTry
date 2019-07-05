c4db342605bb1e04f494048360774f460cfe1f47
omq78 <omar.qab@gmail.com>
2019-07-05 11:21:26 +0300

    Initial Commit

---
+++ TextAnimTry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextAnimTry
+//
+//  Created by Omar Alqabbani on 7/5/19.
+//  Copyright © 2019 OmarALqabbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
